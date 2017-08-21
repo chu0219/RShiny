01b1ff7cd2b5ec253c32b8a63c5c98952b004a7b
Chuyi <chuyi.yang@hymans.co.uk>
2017-08-21 14:41:01 +0100

    Rshinydashboard features - tab box, info box, uploading files

--- rshinydashboard_1.R
+++ rshinydashboard_1.R
@@ -4,13 +4,15 @@ library(shiny)
 library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Basic dashboard"),
+  dashboardHeader(title = "R Shiny Dashboard - test"),
   
   dashboardSidebar(
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
       menuItem("Load data", tabName = "data", icon = icon("database")),
-      menuItem("Charts 1", tabName = "charts1", icon = icon("line-chart")),
+      menuItem("Charts 1", tabName = "charts1", icon = icon("line-chart"),
+               menuSubItem("Sub-item 1", tabName = "subitem1"),
+               menuSubItem("Sub-item 2", tabName = "subitem2")),
       menuItem("Charts 2", tabName = "charts2", icon = icon("bar-chart")),
       menuItem("Some money info", tabName = "tab3", icon = icon("gbp")),
       menuItem("Tables", tabName = "tables", icon = icon("table")),
@@ -24,20 +26,59 @@ ui <- dashboardPage(
     tabItems(
       # First tab - dashboard
       tabItem(tabName = "dashboard",
+              # Title of tab
+              h2("Dashboard"),
+              
+              # Tab box
               fluidRow(
+                tabBox(
+                  title = "Data tab box",
+                  id = "tabbox1", height = "250px",
+                  tabPanel("Male", "Data for males"),
+                  tabPanel("Female", "Data for females")
+                )
+              ),
+              
+              # Info boxes
+              fluidRow(
+                infoBox("Dataset size", 10000, icon = icon("users")),
+                infoBox("Year", "2016 - 2017", icon = icon("calendar"), color = "orange"),
+                infoBox("Gender", "Male", icon = icon("venus-mars"), color = "green")
               )
       ),
       
       # Second tab - database
       tabItem(tabName = "data",
-              fluidRow()
+              h2("Load Data"),
+              fileInput("DT", label = "Upload data"),
+              conditionalPanel(
+                condition = "output.file_import_ready",
+                selectInput("select", label = "Variable name: age",
+                            choices = c("Age", "PenBand", "PostcodeGroup", "Occupation"))
               )
+              ),
+      
+      
+      # Contact tab
+      tabItem(tabName = "contact",
+              h2("Contact"),
+              p("If you have any problems using this R Shiny app, please contact:"),
+              p("Chuyi Yang"),
+              p("chuyi.yang@hymans.co.uk")
+      )
       
     )
   )
 )
 
 server <- function(input, output) {
+  
+  output$file_import_ready <- reactive({
+    return(!is.null(input$DT))
+  })
+  
+  outputOptions(output, "file_import_ready", suspendWhenHidden = FALSE)
+  
 }
 
 shinyApp(ui, server)
